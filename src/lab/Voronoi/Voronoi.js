0bc0ed146c6301d9e133695b0596ad33bcf087dd
Bhashkar Sharma <bhashkarsharma@gmail.com>
2018-03-21 09:45:45 +0100

    Optimized voronoi

--- src/lab/Voronoi/Voronoi.js
+++ src/lab/Voronoi/Voronoi.js
@@ -3,18 +3,17 @@ import LabPage from '../../components/LabPage'
 import React from 'react'
 import Title from '../../components/Title'
 
+const COLORS = ['#FFB300', '#803E75', '#FF6800', '#A6BDD7', '#C10020', '#CEA262', '#817066',
+'#007D34', '#F6768E', '#00538A', '#FF7A5C', '#53377A', '#FF8E00', '#B32851', '#FFFFFF',
+'#F4C800', '#7F180D', '#93AA00', '#593315', '#F13A13', '#232C16', '#0000FF', '#000000',
+'#FF0000', '#00FF00', '#FFFF00', '#FF00FF', '#FF8080', '#808080', '#800000', '#FF8000']
+
 export default class Voronoi extends React.Component {
     constructor() {
         super()
         this.canvas = null
         this.count = 0
-        this.state = {
-            cells: 10,
-            hyp: 0,
-            nx: [],
-            ny: [],
-            nc: []
-        }
+        this.state = { cells: 10 }
         this.updateSize = this.updateSize.bind(this)
     }
 
@@ -43,6 +42,7 @@ export default class Voronoi extends React.Component {
     }
 
     draw() {
+        this.count = 0
         const date = new Date()
         const ctx = this.canvas.getContext('2d')
         const cells = this.state.cells
@@ -54,19 +54,20 @@ export default class Voronoi extends React.Component {
         for (let i = 0; i < cells; i++) {
             nx.push(this.getRand(imgx))
             ny.push(this.getRand(imgy))
-            nc.push('#' + (Math.random() * 0xFFFFFF << 0).toString(16))
+            nc.push(COLORS[i % COLORS.length])
         }
         const hyp = this.getHyp(imgx - 1, imgy - 1)
-        this.setState({ hyp, nx, ny, nc }, () => {console.log('saved')})
-        const step = imgx/cells/40
-        console.log({ hyp, nx, ny, nc, step, cells })
-        for (let y = 0; y < imgy; y += step) {
-            for (let x = 0; x < imgx; x += step) {
-                const j = this.getColorIndexOld(x, y, hyp, cells, nx, ny)
-                ctx.fillStyle = nc[j]
-                const s = step + 1
-                ctx.fillRect(x, y, s, s)
-            }
+        for (let y = 0; y < imgy; y ++) {
+            let d = {}
+            this.getColorMapForRow(y, hyp, cells, nx, ny, 0, imgx, d)
+            Object.keys(d).forEach(i => {
+                const [start, end] = d[i]
+                ctx.strokeStyle = nc[i]
+                ctx.beginPath()
+                ctx.moveTo(start, y)
+                ctx.lineTo(end, y)
+                ctx.stroke()
+            })
         }
         ctx.fillStyle = 'black'
         for (let i = 0; i < cells; i++) {
@@ -75,11 +76,10 @@ export default class Voronoi extends React.Component {
             ctx.fill()
         }
         ctx.font = '16px bold sans-serif'
-        ctx.fillText(`${new Date() - date}ms`, 10, 20)
-        console.log('filled')
+        ctx.fillText(`${new Date() - date}ms - ${this.count}`, 10, 20)
     }
 
-    getColorIndexOld(x, y, hyp, cells, nx, ny) {
+    getColorKey(x, y, hyp, cells, nx, ny) {
         let dmin = hyp
         let j = -1
         for (let i = 0; i < cells; i++) {
@@ -89,45 +89,42 @@ export default class Voronoi extends React.Component {
                 j = i
             }
         }
-
-        // console.log(j)
         return j
     }
 
-    getColorIndex(x, y) {
-        const hyp = this.state.hyp
-        const cells = this.state.cells
-        const nx = this.state.nx
-        const ny = this.state.ny
-        // console.log({ hyp, nx, ny, cells })
-        let dmin = hyp
-        let j = -1
-        for (let i = 0; i < cells; i++) {
-            const d = this.getHyp(nx[i] - x, ny[i] - y)
-            if (d < dmin) {
-                dmin = d
-                j = i
-            }
-        }
-
-        // console.log(j)
-        return j
-    }
-
-    binSearch(a, min, max) {
-        if (a[min] === a[max]) return [min, max]
-        const mid = min + Math.floor((max - min) / 2)
-        if (a[min] === a[mid]) {
-            return this.binSearch(a, mid, max)
-        } else if (a[mid] === a[max]) {
-            return this.binSearch(a, min, mid)
+    setVal(d, key, min, max) {
+        let a, b
+        if (d[key]) {
+            [a, b] = d[key]
         } else {
-            return [this.binSearch(a, mid, max), this.binSearch(a, min, mid)]
+            a = Number.MAX_SAFE_INTEGER
+            b = -1
         }
+        d[key] = [Math.min(min, a), Math.max(max, b)]
     }
 
-    getColorMapForRow() {
-
+    getColorMapForRow(y, hyp, cells, nx, ny, min, max, d) {
+        const a = this.getColorKey(min, y, hyp, cells, nx, ny)
+        const c = this.getColorKey(max, y, hyp, cells, nx, ny)
+        if (a === c) {
+            this.setVal(d, a, min, max)
+            return
+        }
+        if (max - min <= 1) {
+            this.setVal(d, a, min, min)
+            this.setVal(d, c, max, max)
+            return
+        }
+        const mid = min + Math.floor((max - min) / 2)
+        const b = this.getColorKey(mid, y, hyp, cells, nx, ny)
+        if (a === b) {
+            this.getColorMapForRow(y, hyp, cells, nx, ny, mid, max, d)
+        } else if (b === c) {
+            this.getColorMapForRow(y, hyp, cells, nx, ny, min, mid, d)
+        } else {
+            this.getColorMapForRow(y, hyp, cells, nx, ny, mid, max, d)
+            this.getColorMapForRow(y, hyp, cells, nx, ny, min, mid, d)
+        }
     }
 
     render() {
@@ -140,3 +137,4 @@ export default class Voronoi extends React.Component {
         )
     }
 }
+
